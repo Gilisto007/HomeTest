fe8c9bef379160fbfb85355f5c80ed1c0c044543
Kerens <keren.gady@gmail.com>
2017-05-06 13:47:09 +0300

    Initial commit

---
+++ src/main/java/Questions.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.*;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by Kerens on 06/05/2017.
+ */
+public class Questions {
+
+    static String keyword;
+    static String url;
+
+    public static void main (String[] args){
+        question1();
+        question2();
+    }
+
+    public static void question1(){
+        WebDriver driver = null;
+        By searchField = By.cssSelector("#lst-ib");
+
+        keyword = getKeyword();
+        driver = baseClass.setDriver("chrome", "http://google.com");
+        //driver.navigate().to("http://google.com");
+        // TBD - might want to consider wait for site to load function here
+
+        WebElement element = driver.findElement(searchField);
+        element.sendKeys(keyword);
+        element.sendKeys(Keys.RETURN);
+
+        // driver.quit(); - Commented for testing
+    }
+
+    //This one was a bit of a ponder. I believe I found a way to download the assets.
+    //The log and timestamps should provide the download time of the assets
+    //https://www.gnu.org/software/wget/manual/wget.html#Logging-and-Input-File-Options
+    public static void question2(){
+        url = getURL();
+        try {
+            execCmd("wget --page-requisites  -o log --timestamping " + url); //need to install the wget locally
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void execCmd(String cmd) throws java.io.IOException {
+        Runtime.getRuntime().exec(cmd);
+    }
+
+    private static void executeScript(String script, WebDriver driver) {
+        ((JavascriptExecutor) driver).executeScript(script);
+    }
+
+    private static String getKeyword(){
+        System.out.println("Please type your keyword");
+        Scanner sc = new Scanner(System.in);
+        return keyword = sc.nextLine();
+    }
+
+    private static String getURL(){
+        System.out.println("Please type your URL");
+        Scanner sc = new Scanner(System.in);
+        return keyword = sc.nextLine();
+    }
+}
